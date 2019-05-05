af6123151a8d0fa49ef4535f18dcbb3f07ce76d5
subham17 <subham.1993.17@gmail.com>
2019-05-05 15:30:10 +0530

    adding new carousel component

--- force-app/main/default/lwc/zomato/zomato.js
+++ force-app/main/default/lwc/zomato/zomato.js
@@ -1,9 +1,9 @@
-import { LightningElement, track, wire } from 'lwc';
+/* eslint-disable no-console */
+import { LightningElement, track, wire} from 'lwc';
 import getLocation from '@salesforce/apex/zomatoClass.getLocation';
+import searchRestaurants from '@salesforce/apex/zomatoClass.searchRestaurants';
 import { CurrentPageReference } from 'lightning/navigation';
-
-/** The delay used when debouncing event handlers before invoking Apex. */
-const DELAY = 300;
+import { fireEvent } from 'c/pubsub';
 
 export default class Zomato extends LightningElement {
     location = '';
@@ -12,23 +12,93 @@ export default class Zomato extends LightningElement {
     entityType = '';
     restaurant = '';
 
-    @track searchKey = '';
-
     @wire(CurrentPageReference) pageRef;
 
-    @wire(getLocation, { locationName: '$searchKey' })
-    location;
-
-
     handleLocationChange(event) {
-        // Debouncing this method: Do not update the reactive property as long as this function is
-        // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
-        window.clearTimeout(this.delayTimeout);
-        const searchKey = event.target.value;
-        // eslint-disable-next-line @lwc/lwc/no-async-operation
-        this.delayTimeout = setTimeout(() => {
-            this.searchKey = searchKey;
-        }, DELAY);
+        this.location = event.target.value;
+        console.log('location', this.location);
     }
 
+    handleRestaurantChange(event) {
+        this.restaurant = event.target.value;
+        //console.log('restaurant', this.restaurant);
+    }
+
+    selectLocation() {
+
+        getLocation({ 'locationName' : this.location })
+        .then(result => {
+            const output = JSON.parse(result);
+            this.error = undefined
+            console.log("Location::::", output);
+
+            if(output !== undefined) {
+                this.entityId = output[0].entity_id;
+                this.entityType = output[0].entity_type;
+                this.selectedLocation = output[0].title;
+                console.log('this.selectedLocation',this.selectedLocation+this.entityId+this.entityType);
+                /*this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Account created',
+                        variant: 'success',
+                    }),
+                );*/
+            }
+            else {
+                console.log("No info returned in call back in selectLocation method");
+            }
+        })
+        .catch(error => {
+            this.message = undefined;
+            this.error = error;
+            /*this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error creating record',
+                    message: error.body.message,
+                    variant: 'error',
+                }),
+            );*/
+            console.log("error", JSON.stringify(this.error));
+        });
+    }
+
+    searchRestaurant() {
+
+        console.log('restaurant selection data',this.restaurant+''+this.entityId+''+this.entityType);
+
+        searchRestaurants({ 'entityId' : this.entityId, 'entityType' : this.entityType, 'searchTerm' : this.restaurant})
+        .then(result => {
+            this.message = JSON.parse(result);
+            this.error = undefined
+            console.log("Restaurant List::::", this.message);
+
+            if(this.message !== undefined) {
+                console.log("searchRestaurants pubSub fires....");
+                fireEvent(this.pageRef, 'restaurantListUpdate', this.message);
+                /*this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Account created',
+                        variant: 'success',
+                    }),
+                );*/
+            }
+            else {
+                console.log("No info returned in call back in searchRestaurant method");
+            }
+        })
+        .catch(error => {
+            this.message = undefined;
+            this.error = error;
+            /*this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error creating record',
+                    message: error.body.message,
+                    variant: 'error',
+                }),
+            );*/
+            console.log("error", JSON.stringify(this.error));
+        });
+    }
 }
